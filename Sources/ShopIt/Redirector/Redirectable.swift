1d56f14d04af18b7aacb90eb4b5212d312e400d8
enebin <enebin777@gmail.com>
2025-03-21 21:10:25 +0900

    Add `~~Redirectable` protocols

---
+++ Sources/ShopIt/Redirector/Redirectable.swift
@@ -0,0 +1,23 @@
+//
+//  Redirectable.swift
+//  ShopIt
+//
+//  Created by Kai Lee on 3/21/25.
+//
+
+import SwiftUI
+
+/// A type that can be redirected to a scheme URL.
+public protocol SchemeRedirectable {
+    /// Optional URL configuration for app-specific scheme redirection.
+    var schemeUrl: QueryableURL { get }
+}
+
+/// A type that can be redirected to a web URL.
+public protocol WebRedirectable {
+    /// URL configuration for web page redirection.
+    var webUrl: QueryableURL { get }
+}
+
+/// A type that can redirect to either a scheme URL or a web URL.
+public typealias Redirectable = SchemeRedirectable & WebRedirectable
