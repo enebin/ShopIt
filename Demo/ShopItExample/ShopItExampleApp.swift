04ce051c14d4a40ad327abb083fa2621761ddf94
enebin <enebin777@gmail.com>
2025-03-21 22:17:23 +0900

    Add demo app

---
+++ Demo/ShopItExample/ShopItExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopItExampleApp.swift
+//  ShopItExample
+//
+//  Created by Kai Lee on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopItExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
