bc097a9932f140b4cb70706e1f99a0c422c33664
enebin <enebin777@gmail.com>
2025-03-21 21:09:50 +0900

    Add error enumeration

---
+++ Sources/ShopIt/ShopItError.swift
@@ -0,0 +1,14 @@
+//
+//  ShopItError.swift
+//  ShopIt
+//
+//  Created by Kai Lee on 3/21/25.
+//
+
+import Foundation
+
+enum ShopItError: Error {
+    case invalidURL
+    case openUrlActionNotSet
+    case cannotOpenURL
+}
