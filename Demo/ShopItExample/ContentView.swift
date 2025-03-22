c1f770e42bee3ce5c88ed161e703431c14f70706
enebin <enebin777@gmail.com>
2025-03-22 17:27:39 +0900

    Add demo

--- Demo/ShopItExample/ContentView.swift
+++ Demo/ShopItExample/ContentView.swift
@@ -9,20 +9,52 @@ import SwiftUI
 import ShopIt
 
 struct ContentView: View {
-    @Environment(\.openURL) var openURL
-    @Environment(\.shopitRedirector) var redirector
+    @Environment(\.openURL) private var openURL
+    @Environment(\.shopitRedirector) private var redirector
+    
+    private let productName = "iPhone 16 pro"
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            // Product Image (Placeholder image)
+            Image("banner")
+                .resizable()
+                .scaledToFit()
+            
+            Text(productName)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.horizontal)
+            
+            // You can use redirector to redirect to different shopping websites
+            Button("Buy on Amazon") {
+                Task {
+                    try await redirector.redirect(keyword: productName, to: .amazonShopping)
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            // It offers a more convenient way to redirect with predefined redirection
+            Button("Buy on eBay") {
+                Task {
+                    try await redirector.redirect(keyword: productName, to: .ebay)
+                    
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            // You can also use the `RedirectButton` which is a custom button that handles redirection.
+            // It also registers the `openURL` action inside automatically.
+            RedirectButton(keyword: productName, redirection: .walmart) {
+                Text("Buy on Walmart")
+            }
+            .buttonStyle(.borderedProminent)
+
+            Spacer()
         }
-        .padding()
         .task {
+            // !!!: YOU MUST register the openURL action first
             redirector.register(openURL)
-            try? await redirector.redirect(keyword: "Apple", to: .googleSearch)
         }
     }
 }
@@ -30,3 +62,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
