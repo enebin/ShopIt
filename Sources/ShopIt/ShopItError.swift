ca179e76c2f589f2ddac18f5abf73c13ec6cacc9
enebin <enebin777@gmail.com>
2025-03-22 17:26:44 +0900

    Add error description

--- Sources/ShopIt/ShopItError.swift
+++ Sources/ShopIt/ShopItError.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 
-enum ShopItError: Error {
+enum ShopItError: LocalizedError {
     case invalidURL
     case openUrlActionNotSet
     case cannotOpenURL
+}
+
+extension ShopItError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+        case .openUrlActionNotSet:
+            return "Open URL action is not set. Please set it with `ShopitRedirector.register` before using."
+        case .cannotOpenURL:
+            return "Cannot open URL"
+        }
+    }
 }
